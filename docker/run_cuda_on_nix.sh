195f53053e7dbff049bac63eeef89b346355b001
Jacob Ogle <jakeogle@umich.edu>
2025-10-31 13:37:30 -0400

    docker env with nvcc for testing compilation

---
+++ docker/run_cuda_on_nix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the host directory to mount
+HOST_DIR=$HOME/dev/repos/cuda/docker/cudadev
+
+# Set the container path
+CONTAINER_DIR=/workspace
+
+# Docker image name
+IMAGE_NAME=cudadev:latest
+
+# Run the container
+docker run -it --rm -v "${HOST_DIR}:${CONTAINER_DIR}" "${IMAGE_NAME}"
